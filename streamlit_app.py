02b87b168fa247aa676c48fdf8415989f9b3ddd5
Haytham IDRISSI GARTOUMI <48799851+HaythamG@users.noreply.github.com>
2023-01-03 18:38:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit as st 
-st.title("Snowflake App for learning")
-st.header("Uploading data")
-st.markdown("the job is to build an app to upload data from csv files to snowflake databases")
+st.title("My parents New Healthy Diner")
+st.header("Breakfast Menu")
+st.markdown("meal 1")
+st.markdown("meal 2")
+st.text("meal 3")
+
