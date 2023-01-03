7d17d25c5d5ef715b58c590b9056f70231b3baa7
Haytham IDRISSI GARTOUMI <48799851+HaythamG@users.noreply.github.com>
2023-01-03 18:57:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit as st 
+
+st.title("My Mom's Breakfast Menu")
+
 st.header(' Breakfast Menu')
 st.text('🥣 Omega 3 & Blueberry Oatmeal')
 st.text('🥗 Kale, Spinach & Rocket Smoothie')
