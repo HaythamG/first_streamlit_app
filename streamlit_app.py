96b6d634d3abdd84ec12207ffe8d9e091699a7f3
Haytham IDRISSI GARTOUMI <48799851+HaythamG@users.noreply.github.com>
2023-01-03 18:39:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit as st 
 st.title("My parents New Healthy Diner")
 st.header("Breakfast Menu")
-st.markdown("meal 1")
-st.markdown("meal 2")
-st.text("meal 3")
+st.markdown("meal 1:")
+st.text("Product 1, Product 2, Product 3")
+st.markdown("meal 2:")
+st.text("Product 1, Product 2, Product 3")
+st.markdown("meal 3:")
+st.text("Product 1, Product 2, Product 3")
+
 
